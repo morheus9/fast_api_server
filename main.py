edd47c34a1f416ba7a7cabe98d22c1f5ec253ad7
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2021-12-29 11:19:45 +0300

    added docker

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI()  # initialize of server on http://127.0.0.1:8000
 
 templates = Jinja2Templates(directory='templates')  # defines the "templates" directory for Jinja2
 app.mount("/static", StaticFiles(directory="static"), name="static")  # defines the "static" directory
 
 
 @app.get('/', response_class=HTMLResponse)
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})  # returns an HTML page
