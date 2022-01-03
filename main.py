1ec5187d475fe1140bd78bbc5d960726eecd4d9e
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-01-03 15:19:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ app = FastAPI()  # initialize of server on http://127.0.0.1:8000
 templates = Jinja2Templates(directory='templates')  # defines the "templates" directory for Jinja2
 app.mount("/static", StaticFiles(directory="static"), name="static")  # defines the static
 
-"""
-returns an HTML page
-"""
+#returns an HTML page
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
